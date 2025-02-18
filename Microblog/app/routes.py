753623cd7c7c7412ecfce097c1a2915b2f5e7f2f
nopead <smirnov.smirnov2003@yandex.ru>
2025-02-18 09:25:26 +0300

    returned flash message in register route

--- Microblog/app/routes.py
+++ Microblog/app/routes.py
@@ -40,7 +40,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(login=form.login.data).first()
         if user is None or not user.check_password(form.password.data):
-            #flash('Invalid login or password')
+            flash('Invalid login or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
